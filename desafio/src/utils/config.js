ffa5c90d5a0dd331fed8884af5980efc50384501
Lautaro Chaar <lautarochaar@hotmail.com>
2023-03-03 12:59:06 -0300

    Tercera entrega

---
+++ desafio/src/utils/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    
+    isAdmin: true,
+
+    atlas: {
+        strConn: `mongodb+srv://Lautaro:batman123@cluster0.jfywafn.mongodb.net/proyectoFinal?retryWrites=true&w=majority`
+    },
+
+
+}
